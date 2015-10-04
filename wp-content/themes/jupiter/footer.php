1a59f91dc85b9b1e92974da72e9b54154983a8cd
David Maust <david@dmaust.net>
2015-10-04 16:13:12 -0700

    Adding snowplot example with my collector.

--- wp-content/themes/jupiter/footer.php
+++ wp-content/themes/jupiter/footer.php
@@ -395,6 +395,19 @@ var google_remarketing_only = true;
 <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/961150407/?value=1.00&amp;currency_code=USD&amp;label=pG4yCOLe714Qx_unygM&amp;guid=ON&amp;script=0"/>
 </div>
 </noscript>
+<script type="text/javascript">
+;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
+p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
+};p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
+n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","//d26vmwbvym8n3p.cloudfront.net/sp2.js","snowplow"));
+
+window.snowplow('newTracker', 'cf', 'snowplow-clojure.elasticbeanstalk.com', { // Initialise a tracker
+  appId: '1',
+  cookieDomain: 'lessig-campaign.dmaust.net'
+});
+
+window.snowplow('trackPageView');
+</script>
 
 <?php 
 $url_check = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
